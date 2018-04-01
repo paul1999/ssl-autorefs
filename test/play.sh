af71c4bf6a90953bbf0f4ab0df471a4882ebf167
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-04-01 10:37:03 +0200

    Update ssl-logtools

--- test/play.sh
+++ test/play.sh
@@ -2,7 +2,6 @@
 
 # https://www.dropbox.com/sh/yc4wyfl91mtwu6e/AAAKTBVIhKioZoVnQZOoRsSCa?dl=0
 logfile=2017-07-27-erforce-src.log.gz
-logfile=2017-07-27-mct-parsians.log.gz
 if [ ! -f $logfile ]; then
 	wget "https://www.dropbox.com/sh/yc4wyfl91mtwu6e/AADm6JnY2pGOu2hIHf_eFxuva/2017-07-27-erforce-src.log.gz"
 	logfile=2017-07-27-erforce-src.log.gz
