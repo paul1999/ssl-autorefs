b7f65866b1e50ad089e5cfd30ff1e5bf7b9e99fc
Nicolai Ommer <nicolai.ommer@gmail.com>
2019-05-30 21:19:14 +0200

    Remove obsolete test setup

--- test/build.sh
+++ test/build.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# ssl-logtools for log playback
-cd ssl-logtools
-mkdir -p build
-cd build
-cmake ..
-make
-cd ../..
-
-# ssl-refbox
-cd ssl-refbox
-make
-cd ..
-
 # autoref evaluator and recorder
 go get -u github.com/RoboCup-SSL/ssl-go-tools/autorefevaluator
 go get -u github.com/RoboCup-SSL/ssl-go-tools/autoreflogger
